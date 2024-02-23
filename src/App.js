b8be94eea81bf781574a36906e80876711512e4c
oumaimagd <oumaimagdiri98@gmail.com>
2024-02-23 10:42:04 +0100

    react-js-checkpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NaveBare from "./components/NaveBare";
+import { Cardes } from "./components/Cardes";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NaveBare />
+      <br />
+      <h1>bootstrap Cards </h1>
+      <br />
+      <Cardes />
     </div>
   );
 }
